3093caede7d2a2e702af1691d5ada9b5b682202b
Daniel Aguilar <dacmail@gmail.com>
2015-07-06 13:10:50 +0200

    Provincia y comercio

--- taxonomy-comercio.php
+++ taxonomy-comercio.php
@@ -3,32 +3,40 @@
 	<div class="container">
 		<div class="row">
 			<div id="content" class="col-sm-7">
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php $comercio = get_term(get_queried_object()->term_id, 'comercio' ); ?>
-					<h1 class="title nm"><?php echo single_term_title(); ?></h1>
-					<?php if(function_exists("kk_star_ratings")) : echo kk_star_ratings(viviendu_post_id('comercio',get_queried_object()->term_id)); endif; ?>
-					<div class="text main">
-						<?php echo viviendu_get_paragraph(apply_filters('the_content',$comercio->description)); ?>
-						<h2 class="title">Secciones</h2>
-						<div class="row">
-							<?php $related = new WP_Query(array(
-											'posts_per_page' => -1,
-											'tax_query' => array(
-												array(
-													'taxonomy' => 'comercio',
-													'field'    => 'ID',
-													'terms'    => $comercio->term_id
-												),
-											'posts_per_archive_page' => -1,
-											'orderby' => 'rand'
+				<?php $comercio = get_term(get_queried_object()->term_id, 'comercio' ); ?>
+				<h1 class="title nm"><?php echo single_term_title(); ?></h1>
+				<?php if(function_exists("kk_star_ratings")) : echo kk_star_ratings(viviendu_post_id('comercio',get_queried_object()->term_id)); endif; ?>
+				<div class="text main">
+					<?php echo viviendu_get_paragraph(apply_filters('the_content',$comercio->description)); ?>
+					<h2 class="title mini tit-sep"><?php echo single_term_title(); ?> te ofrece</h2>
+					<div class="row">
+						<?php $related = new WP_Query(array(
+										'posts_per_page' => -1,
+										'tax_query' => array(
+											array(
+												'taxonomy' => 'comercio',
+												'field'    => 'ID',
+												'terms'    => $comercio->term_id
 											),
-										)); ?>
-							<?php include(locate_template('templates/sections.php')); ?>
-						</div>
-						<?php echo viviendu_get_paragraph(apply_filters('the_content',$comercio->description), false); ?>
+										'posts_per_archive_page' => -1,
+										'orderby' => 'rand'
+										),
+									)); ?>
+						<?php include(locate_template('templates/sections.php')); ?>
 					</div>
-					<?php break; ?>
-				<?php endwhile; ?>
+					<?php echo viviendu_get_paragraph(apply_filters('the_content',$comercio->description), false); ?>
+				</div>
+				<div class="row">
+					<?php $related = new WP_Query(array(
+									'posts_per_page' => 3,
+									'posts_per_archive_page' => 3,
+									'orderby' => 'rand'
+								)); ?>
+					<?php if ($related->post_count>0): ?>
+						<div class="col-sm-12"><h2 class="title mini tit-sep">Catálogos relacionados con <?php echo $comercio->name; ?></h2></div>
+						<?php include(locate_template('templates/related.php')); ?>
+					<?php endif ?>
+				</div>
 			</div>
 			<?php get_sidebar('comercio'); ?>
 		</div>
--- taxonomy-provincia.php
+++ taxonomy-provincia.php
@@ -3,21 +3,37 @@
 	<div class="container">
 		<div class="row">
 			<div id="content" class="col-sm-7">
-				<?php while ( have_posts() ) : the_post(); ?>
-					<?php $provincia = get_term(get_queried_object()->term_id, 'provincia' ); ?>
-					<h1 class="title nm"><?php echo single_term_title(); ?></h1>
-					<?php if(function_exists("kk_star_ratings")) : echo kk_star_ratings(viviendu_post_id('provincia',get_queried_object()->term_id)); endif; ?>
-					<div class="text main">
-						<?php echo viviendu_get_paragraph(apply_filters('the_content',$provincia->description)); ?>
-						<h2 class="title">Secciones</h2>
-						<div class="row">
-							
-							<?php include(locate_template('templates/sections-provincias.php')); ?>
-						</div>
-						<?php echo viviendu_get_paragraph(apply_filters('the_content',$provincia->description), false); ?>
+				<?php $provincia = get_term(get_queried_object()->term_id, 'provincia' ); ?>
+				<h1 class="title nm"><?php echo single_term_title(); ?></h1>
+				<div class="text main">
+					<?php echo viviendu_get_paragraph(apply_filters('the_content',$provincia->description)); ?>
+					<h2 class="title mini tit-sep">En <?php echo single_term_title(); ?> puedes encontrar:</h2>
+					<div class="row">
+						
+						<?php include(locate_template('templates/sections-provincias.php')); ?>
 					</div>
-					<?php break; ?>
-				<?php endwhile; ?>
+					<?php echo viviendu_get_paragraph(apply_filters('the_content',$provincia->description), false); ?>
+				</div>
+				<div class="row">
+					<?php $related = new WP_Query(array(
+									'posts_per_page' => 3,
+									'tax_query' => array(
+										array(
+											'taxonomy' => 'provincia',
+											'field'    => 'ID',
+											'terms'    => $provincia->term_id,
+										),
+									'posts_per_archive_page' => 3,
+									'orderby' => 'rand'
+									),
+								)); ?>
+					<?php if ($related->post_count>0): ?>
+						<div class="col-sm-12"><h2 class="title mini tit-sep">Catálogos destacados <?php echo $provincia->name; ?></h2></div>
+						<?php include(locate_template('templates/related.php')); ?>
+					<?php endif ?>
+				</div>
+				
+					
 			</div>
 			<?php get_sidebar('provincia'); ?>
 		</div>
