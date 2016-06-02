e7e851a4dd22dc46269df21a5603b1537dc6458e
Daniel Aguilar <dacmail@gmail.com>
2016-06-02 18:43:00 +0200

    FB pixel

--- footer.php
+++ footer.php
@@ -23,6 +23,23 @@
 		ga('create', 'UA-65973785-1', 'auto');
 		ga('send', 'pageview');
 	</script>
+	<!-- Facebook Conversion Code for Entradas -->
+	<script>
+		(function() {
+			var _fbq = window._fbq || (window._fbq = []);
+			if (!_fbq.loaded) {
+				var fbds = document.createElement('script');
+				fbds.async = true;
+				fbds.src = 'https://connect.facebook.net/en_US/fbds.js';
+				var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(fbds, s);
+				_fbq.loaded = true;
+			}
+		})();
+		window._fbq = window._fbq || [];
+		window._fbq.push(['track', '6022865114446', {'value':'0.00','currency':'EUR'}]);
+	</script>
+	<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6022865114446&amp;cd[value]=0.00&amp;cd[currency]=EUR&amp;noscript=1" /></noscript>
 	<?php wp_footer(); ?>
 </body>
 </html>
