4c9b65fee334ca5d29617cd4ae99a6db385b9b1a
Daniel Aguilar <dacmail@gmail.com>
2017-05-16 18:47:28 +0200

    imagenes y js

--- wp-content/themes/viviendu/inc/actions.php
+++ wp-content/themes/viviendu/inc/actions.php
@@ -27,11 +27,9 @@
     wp_enqueue_style('viviendu-style', get_stylesheet_uri() );
 
     if( !is_admin()){
-      wp_deregister_script('jquery');
 
-      wp_enqueue_script('jquery','/wp-includes/js/jquery/jquery.js','','',true);
-      wp_enqueue_script('cycle-js', get_template_directory_uri() . '/js/cycle.js', array('jquery'), '2.0.1', true );
-      wp_enqueue_script('ungrynerd-js', get_template_directory_uri() . '/js/default.js', array('jquery'), '1.0.0', true );
+      wp_enqueue_script('cycle-js', asset_path('scripts/cycle.js'), array('jquery'), '2.0.1', true );
+      wp_enqueue_script('viviendu-js', asset_path('scripts/main.js'), array('jquery'), null, true);
       wp_enqueue_script('addthis', '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-566e723b50e69a9e', '', '', true );
       wp_enqueue_script('html5shiv', 'https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js', array(), '3.7.2');
       wp_enqueue_script('respond', 'https://oss.maxcdn.com/respond/1.4.2/respond.min.js', array(), '1.4.2');
