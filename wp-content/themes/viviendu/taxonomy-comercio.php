0e12877346c92606ed34d3cf38570110d51e4db0
Daniel Aguilar <dacmail@gmail.com>
2022-03-03 15:28:04 +0100

    premium stamp in company profile

--- wp-content/themes/viviendu/taxonomy-comercio.php
+++ wp-content/themes/viviendu/taxonomy-comercio.php
@@ -2,38 +2,45 @@
 <div id="container" class="comercio-seccion section">
 	<div class="container">
 		<div class="row">
-			<div id="content" class="col-sm-7">
-				<?php $comercio = get_term(get_queried_object()->term_id, 'comercio' ); ?>
+			<?php $comercio = get_term(get_queried_object()->term_id, 'comercio'); ?>
+			<?php $premium = get_field('premium', 'comercio_' . $comercio->term_id) ?>
+			<div id="content" class="col-sm-7 <?php echo $premium ? 'premium-page' : '' ?>">
+				<?php if ($premium) : ?>
+					<span class="premium-stamp">Selección <br> viviendu</span>
+				<?php endif; ?>
 				<h1 class="title nm"><?php echo single_term_title(); ?></h1>
-				<?php if(function_exists("kk_star_ratings")) : echo kk_star_ratings(viviendu_post_id('comercio',get_queried_object()->term_id)); endif; ?>
+				<?php if (function_exists("kk_star_ratings")) : echo kk_star_ratings(viviendu_post_id('comercio', get_queried_object()->term_id));
+				endif; ?>
 				<div class="text main">
-					<?php echo viviendu_get_paragraph(apply_filters('the_content',$comercio->description)); ?>
+					<?php echo viviendu_get_paragraph(apply_filters('the_content', $comercio->description)); ?>
 					<h2 class="title mini tit-sep"><?php echo single_term_title(); ?> te ofrece</h2>
 					<div class="row">
 						<?php $related = new WP_Query(array(
-										'posts_per_page' => -1,
-										'tax_query' => array(
-											array(
-												'taxonomy' => 'comercio',
-												'field'    => 'ID',
-												'terms'    => $comercio->term_id
-											),
-										'posts_per_archive_page' => -1,
-										'orderby' => 'rand'
-										),
-									)); ?>
+							'posts_per_page' => -1,
+							'tax_query' => array(
+								array(
+									'taxonomy' => 'comercio',
+									'field'    => 'ID',
+									'terms'    => $comercio->term_id
+								),
+								'posts_per_archive_page' => -1,
+								'orderby' => 'rand'
+							),
+						)); ?>
 						<?php include(locate_template('templates/sections.php')); ?>
 					</div>
-					<?php echo viviendu_get_paragraph(apply_filters('the_content',$comercio->description), false); ?>
+					<?php echo viviendu_get_paragraph(apply_filters('the_content', $comercio->description), false); ?>
 				</div>
 				<div class="row">
 					<?php $related = new WP_Query(array(
-									'posts_per_page' => 3,
-									'posts_per_archive_page' => 3,
-									'orderby' => 'rand'
-								)); ?>
-					<?php if ($related->post_count>0): ?>
-						<div class="col-sm-12"><h2 class="title mini tit-sep">Catálogos relacionados con <?php echo $comercio->name; ?></h2></div>
+						'posts_per_page' => 3,
+						'posts_per_archive_page' => 3,
+						'orderby' => 'rand'
+					)); ?>
+					<?php if ($related->post_count > 0) : ?>
+						<div class="col-sm-12">
+							<h2 class="title mini tit-sep">Catálogos relacionados con <?php echo $comercio->name; ?></h2>
+						</div>
 						<?php include(locate_template('templates/related.php')); ?>
 					<?php endif ?>
 				</div>
